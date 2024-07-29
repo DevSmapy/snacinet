a46f5d1fc773d9ce6b31312d1aa7a3a7a4863a7a
DevSmapy <lyk456@naver.com>
2024-07-29 21:56:43 +0900

    feat: Add `login_view` function

--- snacinet/accounts/views.py
+++ snacinet/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-# Create your views here.
+@api_view(["POST"])
+def login_view(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        })
+    else:
+        return Response({"Error": "Invalid credentials"}, status=400)
