8f772b50b54886b040e8ed46aba96294c0975f89
DevSmapy <lyk456@naver.com>
2024-07-31 22:53:55 +0900

    feat: Add and change `register` urls

--- snacinet/accounts/urls.py
+++ snacinet/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main_view, name="main"),
     path("login/", views.login_page_view, name="login_page"),
-    path("api/register/", views.register_view, name="register"),
+    path("register/", views.register_view, name="register"),
+    path("api/register/", views.register_view, name="register_api"),
     path("api/login/", views.login_view, name="login"),
 ]
