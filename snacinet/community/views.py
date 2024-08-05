8a0fb12aa4fd618f6a66b7291540a2cd4ca2d293
DevSmapy <lyk456@naver.com>
2024-08-06 00:27:36 +0900

    feat: Add functions for `Community`

--- snacinet/community/views.py
+++ snacinet/community/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# community/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'community/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = CommentForm()
+    return render(request, 'community/post_detail.html', {'post': post, 'comments': comments, 'form': form})
