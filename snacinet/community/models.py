134915631e1dafef194082fd4736951574d2307f
DevSmapy <lyk456@naver.com>
2024-08-06 12:47:12 +0900

    feat: Add `Post`, `Comment` classes

--- snacinet/community/models.py
+++ snacinet/community/models.py
@@ -1,10 +1,12 @@
+# community/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.ForeignKey(User on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -18,4 +20,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
-
